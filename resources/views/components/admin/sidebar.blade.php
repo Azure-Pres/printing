70736a3dd65b662fad45693fb72b87213dcd2c4b
Er Anupam Kumar <80740494+anupam-vkreate@users.noreply.github.com>
2023-02-24 13:10:48 +0530

    Code module init

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -1,18 +1,18 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
-        <li class="nav-item active">
+        <li class="nav-item {{Request::is('admin')?'active':''}}">
             <a class="nav-link" href="{{ url('/admin') }}">
                 <i class="typcn typcn-device-desktop menu-icon"></i>
                 <span class="menu-title">Dashboard</span>
             </a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item {{Request::is('admin/clients*')?'active':''}}">
             <a class="nav-link" href="{{ url('/admin/clients') }}">
                 <i class="typcn typcn-user menu-icon"></i>
                 <span class="menu-title">Clients</span>
             </a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item {{Request::is('admin/users*')?'active':''}}">
             <a class="nav-link" href="{{ url('/admin/users') }}">
                 <i class="typcn typcn-user menu-icon"></i>
                 <span class="menu-title">Users</span>

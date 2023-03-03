0c1a76ed26f10f003ad87215af642737cb306adc
ChaturvediAman <108720788+aman-vkreate@users.noreply.github.com>
2023-03-03 13:22:00 +0530

    batches minor change

--- resources/views/livewire/admin/batches/manage.blade.php
+++ resources/views/livewire/admin/batches/manage.blade.php
@@ -5,6 +5,7 @@
         </div>
 
         <form class="forms-sample" wire:submit.prevent="modify()">
+
             <div class="col-lg-12 grid-margin stretch-card">
                 <div class="card">
                     <div class="card-header">
@@ -12,29 +13,29 @@
                     </div>
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-sm-6 mb-2">
-                                <div class="form-group">
+                            <div class="col-sm-6 ">
+                                <div class="form-group mb-2">
                                     <label for="batch_code">Batch Code</label>
                                     <input wire:model.defer="batch_code" type="text" class="form-control" id="batch_code" placeholder="Batch code">
                                     <x-basic.message class="text-danger" :message="$errors->has('batch_code')?$errors->first('batch_code'):''"></x-basic.message>
                                 </div>
                             </div>
-                            <div class="col-sm-6 mb-2">
-                                <div class="form-group">
+                            <div class="col-sm-6 ">
+                                <div class="form-group mb-2">
                                     <label for="from_serial_number">From Serial Number</label>
                                     <input wire:model.defer="from_serial_number" type="number" class="form-control" id="from_serial_number" placeholder="From serial number">
                                     <x-basic.message class="text-danger" :message="$errors->has('from_serial_number')?$errors->first('from_serial_number'):''"></x-basic.message>
                                 </div>
                             </div>
-                            <div class="col-sm-6 mb-2">
-                                <div class="form-group">
+                            <div class="col-sm-6 ">
+                                <div class="form-group mb-2">
                                     <label for="to_serial_number">To Serial Number</label>
                                     <input wire:model.defer="to_serial_number" type="number" class="form-control" id="to_serial_number" placeholder="To serial number">
                                     <x-basic.message class="text-danger" :message="$errors->has('to_serial_number')?$errors->first('to_serial_number'):''"></x-basic.message>
                                 </div>
                             </div>
-                            <div class="col-sm-6 mb-2">
-                                <div class="form-group">
+                            <div class="col-sm-6 ">
+                                <div class="form-group mb-2">
                                     <label for="status">Status</label>
                                     <select wire:model.defer="status" class="form-control" id="status">
                                         <option value="">Select</option>
@@ -63,7 +64,7 @@
                     </div>
                 </div>
             </div> 
+
         </form>
-        
     </div>
 </div>

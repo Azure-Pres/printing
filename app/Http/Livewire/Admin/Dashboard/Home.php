ed260ee96676fd14b226d671526f98fb6eb7fbd0
Er Anupam Kumar <80740494+anupam-vkreate@users.noreply.github.com>
2023-04-12 17:23:01 +0530

    On site changes

--- app/Http/Livewire/Admin/Dashboard/Home.php
+++ app/Http/Livewire/Admin/Dashboard/Home.php
@@ -2,12 +2,44 @@
 
 namespace App\Http\Livewire\Admin\Dashboard;
 
+use App\Models\JobCard;
 use Livewire\Component;
+use Carbon\Carbon;
 
 class Home extends Component
 {
     public function render()
     {
-        return view('livewire.admin.dashboard.home')->layout('layouts.app');
+        $total_jobcards = JobCard::where('status','Active')->where('print_status','!=','Pending')->count();
+        $ready_for_print = JobCard::where('print_status','Ready for Print')->count();
+        $printed_jobcards = JobCard::where('print_status','Printed')->count();
+        $in_progress = JobCard::where('print_status','Work in Progress')->count();
+
+        $today_total_jobcards = JobCard::where('status','Active')->where('print_status','!=','Pending')->whereDate('created_at',Carbon::now())->count();
+        $today_ready_for_print = JobCard::whereDate('created_at',Carbon::now())->where('print_status','Ready for Print')->count();
+        $today_printed_jobcards = JobCard::whereDate('created_at',Carbon::now())->where('print_status','Printed')->count();
+        $today_in_progress = JobCard::whereDate('created_at',Carbon::now())->where('print_status','Work in Progress')->count();
+
+        $month_total_jobcards = JobCard::where('status','Active')->where('print_status','!=','Pending')->count();
+        $month_ready_for_print = JobCard::whereMonth('created_at',Carbon::now())->where('print_status','Ready for Print')->count();
+        $month_printed_jobcards = JobCard::whereMonth('created_at',Carbon::now())->where('print_status','Printed')->count();
+        $month_in_progress = JobCard::whereMonth('created_at',Carbon::now())->where('print_status','Work in Progress')->count();
+
+        $dashboardData = [
+            'total_jobcards' => $total_jobcards,
+            'ready_for_print' => $ready_for_print,
+            'printed_jobcards' => $printed_jobcards,
+            'in_progress' => $in_progress,
+            'today_total_jobcards' => $total_jobcards,
+            'today_ready_for_print' => $ready_for_print,
+            'today_printed_jobcards' => $printed_jobcards,
+            'today_in_progress' => $in_progress,
+            'month_total_jobcards' => $total_jobcards,
+            'month_ready_for_print' => $ready_for_print,
+            'month_printed_jobcards' => $printed_jobcards,
+            'month_in_progress' => $in_progress,
+        ];
+
+        return view('livewire.admin.dashboard.home')->with('dashboardData',$dashboardData)->layout('layouts.app');
     }
 }
--- resources/views/livewire/admin/dashboard/home.blade.php
+++ resources/views/livewire/admin/dashboard/home.blade.php
@@ -1,7 +1,187 @@
 <div class="content-wrapper">
     <div class="row">
-        <div class="col-xl-6 grid-margin stretch-card flex-column">
-            <h5 class="mb-2 text-titlecase mb-4">Welcome to Dashboard</h5>
+        <div class="col-xl-12 mb-0">
+            <h5 class="mb-2 text-titlecase">Welcome to Dashboard</h5>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 mb-2 mt-3">
+            <div class="card">
+                <div class="card-body py-2">
+                    <h4 class="mb-0 text-sm">All Time Summary</h4>   
+                </div>
+            </div>        
+        </div>
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="d-flex align-items-center">
+
+                        <div class="ms-2">
+                            <h4 class="mb-1"> {{$dashboardData['total_jobcards']}} <i class="ti ti-arrow-up-right-circle opacity-50"></i></h4>
+                            <p class="mb-0 opacity-50 text-sm">Total Jobcards</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="d-flex align-items-center">
+
+                        <div class="ms-2">
+                            <h4 class="mb-1"> {{$dashboardData['ready_for_print']}} <i class="ti ti-arrow-up-right-circle opacity-50"></i></h4>
+                            <p class="mb-0 opacity-50 text-sm">Ready for Print</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="d-flex align-items-center">
+
+                        <div class="ms-2">
+                            <h4 class="mb-1"> {{$dashboardData['printed_jobcards']}} <i class="ti ti-arrow-up-right-circle opacity-50"></i></h4>
+                            <p class="mb-0 opacity-50 text-sm">Printed Jobcards</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="d-flex align-items-center">
+
+                        <div class="ms-2">
+                            <h4 class="mb-1"> {{$dashboardData['in_progress']}} <i class="ti ti-arrow-up-right-circle opacity-50"></i></h4>
+                            <p class="mb-0 opacity-50 text-sm">In Progress</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12 mb-2 mt-3">
+            <div class="card">
+                <div class="card-body py-2">
+                    <h4 class="mb-0 text-sm">Today Summary</h4>   
+                </div>
+            </div>        
+        </div>
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="d-flex align-items-center">
+
+                        <div class="ms-2">
+                            <h4 class="mb-1">{{$dashboardData['today_total_jobcards']}}<i class="ti ti-arrow-up-right-circle opacity-50"></i></h4>
+                            <p class="mb-0 opacity-50 text-sm">Total Jobcards</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="d-flex align-items-center">
+
+                        <div class="ms-2">
+                            <h4 class="mb-1">{{$dashboardData['today_ready_for_print']}}<i class="ti ti-arrow-up-right-circle opacity-50"></i></h4>
+                            <p class="mb-0 opacity-50 text-sm">Ready for Print</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="d-flex align-items-center">
+
+                        <div class="ms-2">
+                            <h4 class="mb-1">{{$dashboardData['today_printed_jobcards']}}<i class="ti ti-arrow-up-right-circle opacity-50"></i></h4>
+                            <p class="mb-0 opacity-50 text-sm">Printed Jobcards</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="d-flex align-items-center">
+
+                        <div class="ms-2">
+                            <h4 class="mb-1">{{$dashboardData['today_in_progress']}}<i class="ti ti-arrow-up-right-circle opacity-50"></i></h4>
+                            <p class="mb-0 opacity-50 text-sm">In Progress</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12 mb-2 mt-3">
+            <div class="card">
+                <div class="card-body py-2">
+                    <h4 class="mb-0 text-sm">This Month Summary</h4>   
+                </div>
+            </div>        
+        </div>
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="d-flex align-items-center">
+
+                        <div class="ms-2">
+                            <h4 class="mb-1">{{$dashboardData['month_total_jobcards']}}<i class="ti ti-arrow-up-right-circle opacity-50"></i></h4>
+                            <p class="mb-0 opacity-50 text-sm">Total Jobcards</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="d-flex align-items-center">
+
+                        <div class="ms-2">
+                            <h4 class="mb-1">{{$dashboardData['month_ready_for_print']}}<i class="ti ti-arrow-up-right-circle opacity-50"></i></h4>
+                            <p class="mb-0 opacity-50 text-sm">Ready for Print</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="d-flex align-items-center">
+
+                        <div class="ms-2">
+                            <h4 class="mb-1">{{$dashboardData['month_printed_jobcards']}}<i class="ti ti-arrow-up-right-circle opacity-50"></i></h4>
+                            <p class="mb-0 opacity-50 text-sm">Printed Jobcards</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="d-flex align-items-center">
+
+                        <div class="ms-2">
+                            <h4 class="mb-1">{{$dashboardData['month_in_progress']}}<i class="ti ti-arrow-up-right-circle opacity-50"></i></h4>
+                            <p class="mb-0 opacity-50 text-sm">In Progress</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>

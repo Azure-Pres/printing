b23aec1cfc04d64e67aeda077ea06176b7cfaabf
Er Anupam Kumar <80740494+anupam-vkreate@users.noreply.github.com>
2023-09-11 14:58:20 +0530

    Online verification speed optimized

--- app/Http/Controllers/Api/User/VerificationController.php
+++ app/Http/Controllers/Api/User/VerificationController.php
@@ -57,7 +57,25 @@ class VerificationController extends Controller
             $data = [];
             $code_ids = [];
 
+            $query = "
+            SELECT id, code_data, client_id, batch_id
+            FROM codes
+            WHERE client_id = ? 
+            AND batch_id  = ? 
+            AND (
+                JSON_UNQUOTE(JSON_EXTRACT(code_data, '$.upi_qr_url')) IN (" . implode(', ', array_fill(0, count($input_codes), '?')) . ")
+                OR JSON_UNQUOTE(JSON_EXTRACT(code_data, '$.upistring')) IN (" . implode(', ', array_fill(0, count($input_codes), '?')) . ")
+                OR JSON_UNQUOTE(JSON_EXTRACT(code_data, '$.intent_string')) IN (" . implode(', ', array_fill(0, count($input_codes), '?')) . ")
+                OR JSON_UNQUOTE(JSON_EXTRACT(code_data, '$.qr_text')) IN (" . implode(', ', array_fill(0, count($input_codes), '?')) . ")
+                )
+            ";
+            $bindings = array_merge([$input['client_id'], $input['batch_id']], $input_codes, $input_codes, $input_codes, $input_codes);
+            $matched_codes = DB::select($query, $bindings);
+
+            $jobcard = $batch->getJobcard;
+
             foreach ($input_codes as $input_code) {
+                $code =$this->getCodesFromMatched($matched_codes, $input_code);
 
                 $create = new Verification;
                 $create->code_data = $input_code;
@@ -65,105 +83,96 @@ class VerificationController extends Controller
                 $create->scanned_by = Auth::id();
                 $create->save();
 
-                $query = "
-                SELECT *
-                FROM codes
-                WHERE client_id = " . $input['client_id'] . "
-                AND batch_id  = " . $input['batch_id'] . "
-                AND (
-                    JSON_UNQUOTE(JSON_EXTRACT(code_data, '$.upi_qr_url')) = '" . $input_code . "'
-                    OR JSON_UNQUOTE(JSON_EXTRACT(code_data, '$.upistring')) = '" . $input_code . "'
-                    OR JSON_UNQUOTE(JSON_EXTRACT(code_data, '$.intent_string')) = '" . $input_code . "'
-                    OR JSON_UNQUOTE(JSON_EXTRACT(code_data, '$.qr_text')) = '" . $input_code . "'
-                    )
-                    LIMIT 1
-                    ";
+                $message = 'Verified';
+                $status = 'Success';
 
-                    $code = DB::selectOne($query);
+                if (!$code) {
+                    $message   = 'Invalid or broken code.';
+                    $status    = 'Failed';
+                } else {
 
-                    $message = 'Verified';
-                    $status = 'Success';
-
-                    if (!$code) {
-                        $message   = 'Invalid or broken code.';
+                    if ($batch->status!='Active') {
+                        $message   = 'Batch not active.';
+                        $status    = 'Failed';
+                    }
+                    
+                    if (!$jobcard) {
+                        $message   = 'Job card not assigned.';
                         $status    = 'Failed';
                     }else{
-
-                        if ($batch->status!='Active') {
-                            $message   = 'Batch not active.';
+                        if ($jobcard->status!='Active') {
+                            $message   = 'Jobcard inactive.';
                             $status    = 'Failed';
                         }
-                        $jobcard = $batch->getJobcard;
-                        if (!$jobcard) {
-                            $message   = 'Job card not assigned.';
-                            $status    = 'Failed';
-                        }else{
-                            if ($jobcard->status!='Active') {
-                                $message   = 'Jobcard inactive.';
-                                $status    = 'Failed';
-                            }
-                            $count = Verification::where('code_id',$code->id)->where('client_id',$client->id)->count();
-                            if ($count>=$jobcard->allowed_copies) {
-                                $message   = 'Maximum copy exceeded.';
-                                $status    = 'Failed';
-                            }
+                        
+                        $count = Verification::where('code_id',$code->id)->where('client_id',$client->id)->count();
 
-                            if ($count==$jobcard->allowed_copies-1) {
-                                array_push($code_ids, $code->id);
-                            }
+                        if ($count>=$jobcard->allowed_copies) {
+                            $message   = 'Maximum copy exceeded.';
+                            $status    = 'Failed';
+                        }
+
+                        if ($count==$jobcard->allowed_copies-1) {
+                            array_push($code_ids, $code->id);
                         }
                     }
-
-                    $create->message   = $message;
-                    $create->status    = $status;
-
-                    if ($status=='Success') {
-                        $create->code_id   = $code->id;
-                    }else{
-                        $request_verified = false;
-                    }
-
-                    $create->save();
-
-                    array_push($data, [
-                        'code_data' => $input_code,
-                        'status'    => $status,
-                        'message'   => $message
-                    ]);
                 }
 
-                if ($request_verified) {
-                    $update_first_verification = Code::whereIn('id',$code_ids)->update([
-                        'first_verification_status' => 'Success'
-                    ]);
+                $create->message   = $message;
+                $create->status    = $status;
+
+                if ($status=='Success') {
+                    $create->code_id   = $code->id;
+                }else{
+                    $request_verified = false;
                 }
 
-                $online_history = OnlineHistory::create([
-                    'history' => json_encode($data)
+                $create->save();
+
+                array_push($data, [
+                    'code_data' => $input_code,
+                    'status'    => $status,
+                    'message'   => $message
                 ]);
-
-                // if (!$request_verified && env('FAILED_ONLINE_VERIFICATION_IP')!='') {
-
-                //     try{
-                //         $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-                //         $ip = env('FAILED_ONLINE_VERIFICATION_IP');
-                //         $port = env('FAILED_ONLINE_VERIFICATION_PORT');
-
-                //         if ($socket === false || !socket_connect($socket, $ip, $port)) {
-                //         } else {
-                //             $sock_data = env('FAILED_ONLINE_VERIFICATION_STRING');
-                //             socket_write($socket, $sock_data, strlen($sock_data));
-                //             socket_close($socket);
-                //         }
-                //     }catch(Exception $e){}
-                // }
-
-                return response([
-                    'success'   => true,
-                    'message'   => 'Success',
-                    'data'      => $data,
-                    'verification_status' => $request_verified
-                ], 200);
             }
+
+            if ($request_verified) {
+                $update_first_verification = Code::whereIn('id',$code_ids)->update([
+                    'first_verification_status' => 'Success'
+                ]);
+            }
+
+            $online_history = OnlineHistory::create([
+                'history' => json_encode($data)
+            ]);
+
+            return response([
+                'success'   => true,
+                'message'   => 'Success',
+                'data'      => $data,
+                'verification_status' => $request_verified
+            ], 200);
         }
     }
+
+    public function getCodesFromMatched($matched_codes, $input_code)
+    {
+        $code = null;
+
+        foreach ($matched_codes as $matched_code) {
+            $codeDataArray = json_decode($matched_code->code_data, true);
+            if (
+                (isset($codeDataArray['upi_qr_url']) && $codeDataArray['upi_qr_url'] == $input_code) ||
+                (isset($codeDataArray['upistring']) && $codeDataArray['upistring'] == $input_code) ||
+                (isset($codeDataArray['qr_text']) && $codeDataArray['qr_text'] == $input_code) ||
+                (isset($codeDataArray['intent_string']) && $codeDataArray['intent_string'] == $input_code)
+            ) {
+                $code = $matched_code;
+                break;
+            }
+        }
+
+        return $code;
+    }
+
+}
